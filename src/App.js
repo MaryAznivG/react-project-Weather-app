32d488fac4c5a6ece42a8f96b6f48e6f765c84ae
MaryAznivG <marygalstyan10@gmail.com>
2023-09-26 21:43:01 -0700

    edit

--- src/App.js
+++ src/App.js
@@ -5,23 +5,28 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <SearchEngine />
-        <footer>
-          This project was coded by{" "}
-          <a href="https://www.linkedin.com/in/aznivgalstyan/" rel="noreferrer">
-            Azniv Mary Galstyan
-          </a>{" "}
-          and is {""}
-          <a
-            href="https://github.com/MaryAznivG/react-project-Weather-app"
-            rel="noreferrer"
-          >
-            open-sourced on Github.
-          </a>
-        </footer>
-      </header>
+      <div className="container">
+        <header className="App">
+          <h1>Weather App</h1>
+          <SearchEngine />
+          <footer>
+            This project was coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/aznivgalstyan/"
+              rel="noreferrer"
+            >
+              Azniv Mary Galstyan
+            </a>{" "}
+            and is {""}
+            <a
+              href="https://github.com/MaryAznivG/react-project-Weather-app"
+              rel="noreferrer"
+            >
+              open-sourced on Github.
+            </a>
+          </footer>
+        </header>
+      </div>
     </div>
   );
 }
