e405d14f6249dbd54c0d171fd39310945d24da9a
MaryAznivG <129576354+MaryAznivG@users.noreply.github.com>
2023-09-29 22:34:23 -0700

    added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-          <Weather.js />
+          <Weather defaultCity="Los Angeles"/>
           <footer>
             This project was coded by{" "}
             <a
