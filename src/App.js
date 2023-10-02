13c130e81c7047a6206122426eb95a2bc49aee91
MaryAznivG <129576354+MaryAznivG@users.noreply.github.com>
2023-10-01 20:54:14 -0700

    editing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from "react";
 import Weather from "./Weather";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-          <Weather defaultCity="Los Angeles"/>
-          <footer>
-            This project was coded by{" "}
-            <a
-              href="https://www.linkedin.com/in/aznivgalstyan/"
-              rel="noreferrer"
-            >
-              Azniv Mary Galstyan
-            </a>{" "}
-            and is {""}
-            <a
-              href="https://github.com/MaryAznivG/react-project-Weather-app"
-              rel="noreferrer"
-            >
-              open-sourced on Github.
-            </a>
-          </footer>
+        <Weather defaultCity="Los Angeles" />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/aznivgalstyan/"
+            rel="noopener noreferrer"
+          >
+            Azniv Mary Galstyan
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/MaryAznivG/react-project-Weather-app"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github.
+          </a>
+        </footer>
       </div>
     </div>
   );
