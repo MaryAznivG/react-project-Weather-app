a75d6d32b22f3f4512e4d01cae2e23ce82f4212f
MaryAznivG <marygalstyan10@gmail.com>
2023-10-08 19:43:20 -0700

    used effect to change the forecast weather icon

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,10 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  // if the coordinates change
+  // set loaded false
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
